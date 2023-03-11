1521b972aee9399414e5e75d258e19bb18b96271
DilshodGaniboyev <ganiboyevdilshod2002@gmail.com>
2023-03-11 19:17:46 +0500

    test

--- src/main/java/com/tafakkoor/e_learn/controller/AuthController.java
+++ src/main/java/com/tafakkoor/e_learn/controller/AuthController.java
@@ -20,6 +20,7 @@ public class AuthController {
         System.out.println("hello from dilshod branch");
         System.out.println("Test 33");
         System.out.println("Nimadir");
+        System.out.println("Nimadir232");
         this.authUserRepository = authUserRepository;
         this.passwordEncoder = passwordEncoder;
 
