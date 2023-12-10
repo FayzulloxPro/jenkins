b393c397705ea6c00aac1870627491777867b5a2
Fayzullo <qahramonovf26@gmail.com>
2023-12-10 05:18:54 +0500

    tests added

---
+++ src/test/java/com/tafakkoor/e_learn/services/ImageServiceTest.java
@@ -0,0 +1,14 @@
+package com.tafakkoor.e_learn.services;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+class ImageServiceTest {
+
+
+    @Test
+    void simpleTest_success(){
+        assertEquals(2+2, 4);
+    }
+  
+}
